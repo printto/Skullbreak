352bd0794c67394890efe36ff780027c54fca067
Puthikanon Non TIX18 <nonputhikanon@kamk.fi>
2019-06-11 10:50:55 +0300

    try singleton scoreManager :sparkles:

--- Assets/_Scripts/Coin.cs
+++ Assets/_Scripts/Coin.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour {
 
+    private ScoreManager score = ScoreManager.getManger();
    
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
     public void OnCollisionEnter(Collision collision) {
         Destroy(gameObject);
+        score.AddScore(1);
+        Debug.Log(score.GetScore());
     }
 }
