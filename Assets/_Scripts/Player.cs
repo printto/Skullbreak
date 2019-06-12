669bed3606e08998729d398eed8f221165eddc66
Borisuth Theeruth TIX18 <theeruthborisuth@kamk.fi>
2019-06-12 15:09:46 +0300

    keep on fixing the control

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -47,7 +47,8 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        
+
+        /*
         foreach (Touch t in Input.touches)
         {
             if(t.phase == TouchPhase.Began)
@@ -80,7 +81,19 @@ public class Player : MonoBehaviour {
                 swiping = false;
             }
         }
+        */
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0); // get first touch since touch count is greater than zero
 
+            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+            {
+                // get the touch position from the screen touch to world point
+                Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
+                // lerp and set the position of the current object to that of the touch, but smoothly over time.
+                transform.position = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime);
+            }
+        }
 
 
         if (Time.time < animationDuration)
