41ebc89935362acdcae2f24709fd931156b3fa62
Borisuth Theeruth TIX18 <theeruthborisuth@kamk.fi>
2019-06-12 15:14:07 +0300

    keep on fixing the control

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -91,7 +91,7 @@ public class Player : MonoBehaviour {
                 // get the touch position from the screen touch to world point
                 Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
                 // lerp and set the position of the current object to that of the touch, but smoothly over time.
-                transform.position = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime);
+                transform.position = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime) * 10;
             }
         }
 
