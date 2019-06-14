e82ae732fddd8a178dbafe081de31a4a7be43755
Borisuth Theeruth TIX18 <theeruthborisuth@kamk.fi>
2019-06-14 13:58:27 +0300

    add new map and fall damage

---
+++ Assets/_Scripts/fallDamage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class fallDamage : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
