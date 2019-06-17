012f8b45082cfa017b5aa3115f91773cab712d0b
Borisuth Theeruth TIX18 <theeruthborisuth@kamk.fi>
2019-06-17 16:34:29 +0300

    Added script for map components

---
+++ Assets/_Scripts/MapGeneratorForComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeneratorForComponent : MonoBehaviour {
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //Debug.Log("Hit something");
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            SpawnMap();
+        }
+
+    }
+
+    // Spawn new map
+    void SpawnMap()
+    {
+
+        if (!transform.parent.gameObject.GetComponent<MapGenerator>().createdNext)
+        {
+            transform.parent.gameObject.GetComponent<MapGenerator>().SpawnMap();
+        }
+
+    }
+
+}
