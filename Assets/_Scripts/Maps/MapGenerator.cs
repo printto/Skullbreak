dc84e4ac71dd13f7416c066460264489750b1eb2
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-06-18 14:46:56 +0300

    Fixed monster slow down buff

--- Assets/_Scripts/Maps/MapGenerator.cs
+++ Assets/_Scripts/Maps/MapGenerator.cs
@@ -13,7 +13,7 @@ public class MapGenerator : MonoBehaviour {
         createdNext = false;
     }
 
-    int countDown = 60 * 10;
+    int countDown = 60 * 30;
 	// Update is called once per frame
 	void FixedUpdate () {
         if ( countDown-- <= 0)
