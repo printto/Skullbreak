2d26380581438890658da6b82ea99cef450e5fae
Printto <printtopipatkasira@gmail.com>
2019-06-20 13:42:46 +0300

    Fully playable camera

--- Assets/_Scripts/Cameras/CameraFollowSmooth.cs
+++ Assets/_Scripts/Cameras/CameraFollowSmooth.cs
@@ -6,19 +6,29 @@ public class CameraFollowSmooth : MonoBehaviour
 {
 
     public Transform target;
+    public Transform holder;
 
     public float smoothSpeed = 0.125f;
     public float yPosRestriction = -1;
     public Vector3 offset;
 
+    private RotationWay currentRotation = RotationWay.STOP;
+    public float cameraYOffset = -0.7070175f;
+
+    enum RotationWay
+    {
+        STOP,
+        LEFT,
+        RIGHT
+    }
+
     void Update()
     {
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 desiredPosition = holder.position;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
         transform.LookAt(target);
-
-
+        //transform.RotateAround(target.transform.position, target.transform.up, 100 * Time.deltaTime);
     }
 
 }
