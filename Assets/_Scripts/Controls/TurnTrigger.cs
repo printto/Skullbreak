7bee0b430aa55d9e38c4cdd5eca2fbce9d3fec5b
Printto <printtopipatkasira@gmail.com>
2019-06-20 14:17:50 +0300

    Saved camera holder to player prefab

---
+++ Assets/_Scripts/Controls/TurnTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnTrigger : MonoBehaviour {
+
+    public float DegreeToTurn = 0;
+    public float Delay = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            StartCoroutine(TurnPlayer(other));
+        }
+    }
+
+    IEnumerator TurnPlayer(Collider collision)
+    {
+        yield return new WaitForSeconds(Delay);
+        Vector3 temp = collision.gameObject.transform.eulerAngles + new Vector3(0, DegreeToTurn, 0);
+        collision.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+        collision.gameObject.GetComponent<Transform>().eulerAngles = temp;
+    }
+
+}
