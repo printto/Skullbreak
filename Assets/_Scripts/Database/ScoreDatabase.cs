d6418c8550b2138e05fb3c7cec79155e62383593
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-06-24 13:52:52 +0300

    Color picker or something?

---
+++ Assets/_Scripts/Database/ScoreDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDatabase : MonoBehaviour {
+
+	public static void store(string name, float score)
+    {
+
+    }
+
+}
