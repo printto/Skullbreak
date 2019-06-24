4c46aefa9b51177db8328a9b69101acea782c584
Borisuth Theeruth TIX18 <theeruthborisuth@kamk.fi>
2019-06-24 11:43:52 +0300

    add pause menu

---
+++ Assets/_Scripts/Menus/pauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+	
+	// Update is called once per frame
+
+
+    public void openPauseMenu()
+    {
+        if(GameIsPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+}
