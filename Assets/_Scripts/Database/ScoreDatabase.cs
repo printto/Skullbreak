53573e7619baa01a14eff7b4c546c98a48156e6b
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-06-24 16:45:08 +0300

    ScoreDatabase now able to push score to database and check for the high score

--- Assets/_Scripts/Database/ScoreDatabase.cs
+++ Assets/_Scripts/Database/ScoreDatabase.cs
@@ -1,13 +1,40 @@
-﻿using Proyecto26;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Proyecto26;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Policy;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class ScoreDatabase : MonoBehaviour {
+public class ScoreDatabase
+{
 
     public static void save(string name, float score)
     {
-        RestClient.Post(Environment.DatabaseURL + ".json", new User(name, score));
+        saveTask(name, score);
+    }
+
+    static void saveTask(string name, float score)
+    {
+        string url = Environment.DatabaseURL + name + ".json";
+        WWW www = new WWW(url);
+        while (!www.isDone)
+        {
+            //Do nothing
+        }
+        try
+        {
+            User dluser = JsonUtility.FromJson<User>(www.text);
+            if (dluser.userscore < score)
+            {
+                RestClient.Put(Environment.DatabaseURL + name + ".json", new User(name, score));
+            }
+        }
+        catch
+        {
+            RestClient.Put(Environment.DatabaseURL + name + ".json", new User(name, score));
+        }
     }
 
 }
