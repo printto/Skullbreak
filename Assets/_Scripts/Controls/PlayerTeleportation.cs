e259ceb3f619fb8866715c2acc544ee9923eee75
NonPN <non9441@gmail.com>
2019-07-01 14:39:36 +0300

    make basic structure script for new teleporting :confused:

---
+++ Assets/_Scripts/Controls/PlayerTeleportation.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleportation : MonoBehaviour {
+    bool isTeleporting;
+    float energy;
+    Player player;
+    
+
+	// Use this for initialization
+	void Start () {
+        isTeleporting = false;
+        energy = 4f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isTeleporting)
+        {
+            RestoreEnergy();
+        }
+	}
+
+    void RestoreEnergy()
+    {
+        if (energy < 4f)
+        {
+            energy += Time.deltaTime;
+        }
+    }
+
+    void StartTeleport ()
+    {
+        if (energy != 0f)
+        {
+            isTeleporting = true;
+        }
+    }
+
+    void StopTeleport ()
+    {
+        isTeleporting = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag.Equals(""))
+        {
+
+        }
+    }
+}
