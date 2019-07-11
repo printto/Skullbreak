1e1f936a57d302670219a04507ddc4dc62f866ab
NonPN <non9441@gmail.com>
2019-07-11 11:39:40 +0300

    add ending gate code waiting for the scene :sparkles:

---
+++ Assets/_Scripts/Menus/EndingGate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingGate : MonoBehaviour {
+
+    public Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        SceneTransition.setAnimator(animator);
+        SceneTransition.setScene(""); 
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player") || other.gameObject.tag.Equals("PlayerFace"))
+        {
+            StartCoroutine(SceneTransition.LoadScene());
+        }
+    }
+}
