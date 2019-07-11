073a0ea81ed6a1b7c65dfb37e3d1aff6cb7dfd75
NonPN <non9441@gmail.com>
2019-07-11 14:42:07 +0300

    fix map17 and add ending gate to it :bug:

--- Assets/_Scripts/Menus/EndingGate.cs
+++ Assets/_Scripts/Menus/EndingGate.cs
@@ -6,16 +6,12 @@ public class EndingGate : MonoBehaviour {
 
     public Animator animator;
 
-    // Use this for initialization
-    void Start () {
-        SceneTransition.setAnimator(animator);
-        SceneTransition.setScene(""); 
-	}
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag.Equals("Player") || other.gameObject.tag.Equals("PlayerFace"))
         {
+            SceneTransition.setAnimator(animator);
+            SceneTransition.setScene("EndingScene");
             StartCoroutine(SceneTransition.LoadScene());
         }
     }
