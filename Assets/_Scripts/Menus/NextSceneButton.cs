157261af9023bab73ec5151d8b47731e04b9bf51
Theeruth Borisuth <theeruth.b@ku.th>
2019-07-11 11:06:57 +0300

    add ending scene

---
+++ Assets/_Scripts/Menus/NextSceneButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneButton : MonoBehaviour {
+
+    public Animator animator;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        SceneTransition.setAnimator(animator);
+
+    }
+
+    public void RetryGame()
+    {
+        SceneTransition.setScene(LoadPrevScene.getLastLevel());
+        SceneTransition.getScene();
+        StartCoroutine(SceneTransition.LoadScene());
+    }
+
+    public void Mainmenu()
+    {
+        SceneTransition.setAnimator(animator);
+        SceneTransition.setScene("Mainmenu");
+        SceneTransition.getScene();
+        StartCoroutine(SceneTransition.LoadScene());
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
