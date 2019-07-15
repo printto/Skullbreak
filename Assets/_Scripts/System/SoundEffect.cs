e08a908b537d9da6c01a82ddb46fb281fd146d2d
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-07-15 13:37:01 +0300

    Added basic sound effects

--- Assets/_Scripts/System/SoundEffect.cs
+++ Assets/_Scripts/System/SoundEffect.cs
@@ -7,50 +7,33 @@ using UnityEngine;
  * This class needs to be attached to the game object along with AudioSource component.
  * 
  */
-public class SoundEffect : MonoBehaviour {
+public class SoundEffect : MonoBehaviour
+{
 
     //Audio sources
     AudioSource audio;
-    public AudioClip[] BGM;
-    public AudioClip[] Lane;
-    public AudioClip[] Jump;
-    public AudioClip[] Hit;
-    public AudioClip[] Coin;
+    public AudioClip[] BGMSounds;
+    public AudioClip[] LaneSounds;
+    public AudioClip[] JumpSounds;
+    public AudioClip[] HitSounds;
+    public AudioClip[] CoinSounds;
+    public AudioClip[] TeleportSounds;
 
     private void Start()
     {
         audio = GetComponent<AudioSource>();
     }
 
-    void PlaySound(AudioClip[] sounds)
+    /*
+     * Put in the audio source in SoundEffect class
+     */
+    public void PlaySound(AudioClip[] sounds)
     {
-        int randomed = Random.Range(0, sounds.Length - 1);
-        audio.PlayOneShot(sounds[randomed]);
-    }
-
-    public void PlayBGM()
-    {
-        PlaySound(BGM);
-    }
-
-    public void PlayLane()
-    {
-        PlaySound(Lane);
-    }
-
-    public void PlayJump()
-    {
-        PlaySound(Jump);
-    }
-
-    public void PlayHit()
-    {
-        PlaySound(Hit);
-    }
-
-    public void PlayCoin()
-    {
-        PlaySound(Coin);
+            if (sounds.Length != 0)
+            {
+                int randomed = Random.Range(0, sounds.Length);
+                audio.PlayOneShot(sounds[randomed]);
+            }
     }
 
 }
